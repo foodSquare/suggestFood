71b14b2264284c16648556d9d4b83bb637e271ca
Omar Suca <omarsucap100@gmail.com>
2015-08-26 20:35:28 +0100

    changing the styles and adding functionality to the header and list

--- source/controllers/list/listCtrl.js
+++ source/controllers/list/listCtrl.js
@@ -1,13 +1,47 @@
 (function(){
-  app.controller('ListCtrl', ['$scope', function($scope) {
-    $scope.foodList = [
-      { title: 'Meals 1', id: 1 },
-      { title: 'Meals 2', id: 2 },
-      { title: 'Meals 3', id: 3 },
-      { title: 'Meals 4', id: 4 },
-      { title: 'Meals 5', id: 5 },
-      { title: 'Meals 6', id: 6 }
-    ];
+	app.controller('ListCtrl', ['$scope', function($scope) {
+		$scope.foodList = [
+	  	{ 
+			title: 'Meals 1', 
+			id: 1,
+			description: 'This is the best restaurant 1'
+	   	},
+	  	{ 
+			title: 'Meals 2', 
+			id: 2,
+			description: 'This is the best restaurant 2'
+	   	},
+	   	{ 
+			title: 'Meals 3', 
+			id: 3,
+			description: 'This is the best restaurant 3'
+	   	},
+	   	{ 
+			title: 'Meals 4', 
+			id: 4,
+			description: 'This is the best restaurant 4'
+	   	},
+	   	{ 
+			title: 'Meals 5', 
+			id: 5,
+			description: 'This is the best restaurant 5'
+	   	},
+	   	{ 
+			title: 'Meals 6', 
+			id: 6,
+			description: 'This is the best restaurant 6'
+	   	},
+	   	{ 
+			title: 'Meals 7', 
+			id: 7,
+			description: 'This is the best restaurant 7'
+	   	},
+	   	{ 
+			title: 'Meals 8', 
+			id: 8,
+			description: 'This is the best restaurant 8'
+	   	},
+	];
 
   }]);
 
